bf76c4a45bbb53acb231127ebb64b358c5bcf4c9
DigitalLeaves <contact@digitalleaves.com>
2013-09-01 10:26:51 +0200

    Initial Commit

---
+++ NSURLTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSURLTest
+//
+//  Created by Nacho on 01/09/13.
+//  Copyright (c) 2013 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
